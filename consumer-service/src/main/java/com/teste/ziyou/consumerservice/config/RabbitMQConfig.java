3f9f50afb9f1345e5b38743eab98b8a82981cfdf
Your NameAndre <farias96andre@gmail.com>
2025-06-18 05:49:37 -0300

    config: criando configuracao rabbitmq

---
+++ consumer-service/src/main/java/com/teste/ziyou/consumerservice/config/RabbitMQConfig.java
@@ -0,0 +1,34 @@
+package com.teste.ziyou.consumerservice.config;
+
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.amqp.core.Queue;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Value("${rabbitmq.queue.name}")
+    private String queueName;
+
+    @Bean
+    public Queue ordersQueue() {
+        return new Queue(queueName, true);
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(final ConnectionFactory connectionFactory) {
+        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jsonMessageConverter());
+        return rabbitTemplate;
+    }
+}
