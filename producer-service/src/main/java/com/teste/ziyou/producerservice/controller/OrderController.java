4ae61bc45302c18d32f558bd9e372ae267089e59
Your NameAndre <farias96andre@gmail.com>
2025-06-18 05:47:46 -0300

    feat: criando classe de recurso para operacao publishPlacedOrderMessage

---
+++ producer-service/src/main/java/com/teste/ziyou/producerservice/controller/OrderController.java
@@ -0,0 +1,23 @@
+package com.teste.ziyou.producerservice.controller;
+
+import com.teste.ziyou.producerservice.model.Order;
+import com.teste.ziyou.producerservice.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @MutationMapping
+    public String publishPlacedOrderMessage(@Argument Order order) {
+        log.info("Requisição recebida para publicar pedido: {}", order.getOrder());
+        return orderService.publishOrder(order);
+    }
+}
