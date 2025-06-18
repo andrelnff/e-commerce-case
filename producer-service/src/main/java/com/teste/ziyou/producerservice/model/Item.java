f768464ad3356b754b2616cd29cf2571303c27b8
Your NameAndre <farias96andre@gmail.com>
2025-06-18 05:45:30 -0300

    feat: criando model item

---
+++ producer-service/src/main/java/com/teste/ziyou/producerservice/model/Item.java
@@ -0,0 +1,18 @@
+package com.teste.ziyou.producerservice.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+
+@Getter
+@Setter
+public class Item {
+    private String name;
+    private String image;
+    private Integer qty;
+    private BigDecimal cost;
+    private String currency;
+}
+
