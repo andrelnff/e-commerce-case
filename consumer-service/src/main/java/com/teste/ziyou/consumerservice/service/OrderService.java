d023fc11be235a34af41444afe61c74387aae653
andre.luiz <andre.luiz@sefa.pa.gov.br>
2025-06-18 11:12:36 -0300

    feat: criando interface para servico

--- consumer-service/src/main/java/com/teste/ziyou/consumerservice/service/OrderService.java
+++ consumer-service/src/main/java/com/teste/ziyou/consumerservice/service/OrderService.java
@@ -1,30 +1,10 @@
 package com.teste.ziyou.consumerservice.service;
 
 import com.teste.ziyou.consumerservice.model.Order;
-import com.teste.ziyou.consumerservice.repository.OrderRepository;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
-@RequiredArgsConstructor
-@Slf4j
-public class OrderService {
-
-    private final OrderRepository orderRepository;
-
-    public void saveOrder(Order order) {
-        log.info("Pedido recebido da fila: {}", order.getOrder());
-        orderRepository.save(order);
-        log.info("Pedido salvo: {}", order.getOrder());
-    }
-
-    public List<Order> findAll() {
-        log.info("Buscando todos os pedidos");
-        List<Order> orders = orderRepository.findAll();
-        log.info("Pedidos encontrados: {}", orders.size());
-        return orders;
-    }
+public interface OrderService {
+    void saveOrder(Order order);
+    List<Order> findAll();
 }
